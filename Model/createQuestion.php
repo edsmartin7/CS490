312c006e331d8dc59d55006d0233446c05eb3695
Edward Martin <em244@afsconnect1.njit.edu>
2017-03-04 19:26:45 -0500

    createQuestion fixed

--- Model/createQuestion.php
+++ Model/createQuestion.php
@@ -1,13 +1,14 @@
 <?php
 
    $difficulty = $_POST['difficulty'];
+   $category = $_POST['category'];
    $question = $_POST['question'];
 
    require_once('config.php');
    extract(dbConfig());
    $db = new mysqli($host, $user, $pw, $sqldb);
-   $query = "INSERT INTO  questions (difficulty, question)
-             VALUES ('$difficulty', '$question')";
+   $query = "INSERT INTO  questions (difficulty, category, question)
+             VALUES ('$difficulty', '$category', '$question')";
       
    $result = $db->query($query); 
    //$row = $result->fetch_assoc();
