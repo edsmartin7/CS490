993f4d2510d1b4249109e334d65db2b3f44e5370
Edward Martin <em244@afsconnect1.njit.edu>
2017-03-15 00:16:52 -0400

    teacher main prelim

--- View/teacherMain.php
+++ View/teacherMain.php
@@ -2,33 +2,50 @@
 <html>
    <head>
       <title>Hello Professor</title>
-       <link rel="stylesheet" type="text/css" href="teacher.css">
+       <link rel="stylesheet" type="text/css" href="teacherstyle.css">
    </head>
    <body>
+      <script src="teacherscript.js"></script>
+      <?php
+      ?>
       <div class="wrapper">
          <div id="qbox">
             <p>Create a new question</p>
+	    <button type="button" onclick="showDiv()">Create Question</button>
 	    <p>Create an exam</p>
+	    <button type="button">Create Exam</button>
          </div>
-         <div id="abox">
+         <div id="abox" style="display:none;">
             <form method="post" action="teacherMain.php">
                <table>
 	          <tr>
+		     <td>Question Type</td>
+		     <td><input type="text" name="questiontype"></td>
+		  </tr>
+	          <tr>
 	             <td>Difficulty</td>
-		     <td><input type="text" name="difficulty"
-		  class="textInput"></td>
+		     <td><input type="text" name="difficulty"></td>
 	          </tr>
 	          <tr>
 	             <td>Question</td>
 		     <td><input type="text" name="question"
 		  class="textInput"></td>
 	          </tr>
+		  <tr>
+		     <td>Test Case 1</td>
+		     <td><input type="text" name="testcase1" ></td>
+		  </tr>
+		  <tr>
+		     <td>Test Case 2</td>
+		     <td><input type="text" name="testcase2" ></td>
+		  </tr>
+		  <tr>
+		     <td></td>
+		     <td><input type="submit" name="submitquestion" value="Add Your Question"></td>
+		  <tr>
 	       </table>
 	    </form>
          </div>
       </div>
-      <?php
-
-      ?>
    </body>
 </html>
---
+++ View/teacherscript.js
@@ -0,0 +1,14 @@
+
+function showDiv(){
+
+   document.getElementById('abox').style.display = "block";
+   
+}
+
+function hideDiv(){
+
+   //hide when question is submitted
+   document.getElementById('abox').style.display = "none";
+
+}
+
